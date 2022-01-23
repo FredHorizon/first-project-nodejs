8ca81ba2c3d3edde9f6699c815dd6a69bf053eae
FredHorizon <fredoliveira.sys@gmail.com>
2022-01-23 13:18:01 -0400

    Configures import in archives ts

---
+++ src/routes/index.ts
@@ -0,0 +1,7 @@
+import { Router } from 'express';
+
+const routes = Router();
+
+routes.get('/', (req, res) => res.json({ message: 'Teste ok 5' }));
+
+export default routes;
--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,9 @@
 import express from 'express';
+import routes from './routes';
 
 const app = express();
 
-app.get('/', (req, res) => res.json({ message: 'Teste ok 4' }));
+app.use(routes); // routes torna-se um middleware agora
 
 app.listen(3333, () => {
   console.log('Server Started on port 3333.');
