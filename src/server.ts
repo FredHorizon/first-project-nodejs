5c213c1242ec5c8d12d1ceb272d7bcaa70574ebf
FredHorizon <fredoliveira.sys@gmail.com>
2022-01-23 13:01:22 -0400

    Configures Eslint

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,9 @@
-import express from 'express'
+import express from 'express';
 
-const app = express()
+const app = express();
 
-app.get('/', (req, res) => {
-  return res.json({ message: 'Teste ok 4' })
-})
+app.get('/', (req, res) => res.json({ message: 'Teste ok 4' }));
 
 app.listen(3333, () => {
-  console.log('Server Started on port 3333.')
-})
+  console.log('Server Started on port 3333.');
+});
